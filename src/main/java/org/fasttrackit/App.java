39ed089aef1706418aba7aae72d59e26bae79b8f
mihai135 <mihai135@yahoo.com>
2019-01-10 19:10:49 +0200

    studied static variables

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -9,24 +9,38 @@ public class App
     public static void main( String[] args )
     {
 
+//        Engine engine = new Engine();
+//        engine.manufacturer = "Renault";
+//        engine.capacity = 1500;
+//
+//        Car car = new Car(engine);
+//        car.setName("Dacia");
+//        car.setColor("white");
+//        car.doorCount = 4;
+//        car.setMileage(7.5);
+//        car.running = true;
+//
+//        double distance = car.accelerate(100, 0.5);
+//
+//
+//        car.engine.manufacturer = "BMW";
+//
+//        Car car2 = new Car(new Engine());
 
-        Engine engine = new Engine();
-        engine.manufacturer = "Renault";
-        engine.capacity = 1500;
 
-        Car car = new Car(engine);
-        car.setName("Dacia");
-        car.setColor("white");
-        car.doorCount = 4;
-        car.setMileage(7.5);
-        car.running = true;
+//      Example of static variables unique for the whole application
 
-        double distance = car.accelerate(100, 0.5);
+        Vehicle vehicle1 = new Vehicle();
+        vehicle1.vehicleCount = 1;
 
+        Vehicle vehicle2 = new Vehicle();
+        vehicle2.vehicleCount = 2;
 
-        car.engine.manufacturer = "BMW";
+        Vehicle.vehicleCount = 3;
 
-        Car car2 = new Car(new Engine());
+        System.out.println("value from vehicle 1: " + vehicle1.vehicleCount);
+        System.out.println("value from vehicle 2: " + vehicle2.vehicleCount);
+        System.out.println("value from Vehicle class: " + Vehicle.vehicleCount);
 
 
 
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -2,9 +2,15 @@ package org.fasttrackit;
 
 public class Vehicle {
 
+    public static int vehicleCount;
+
     private String name;
     private String color;
     private double mileage;
+
+    public Vehicle() {
+        vehicleCount++;
+    }
 
     public double accelerate(double speed, double durationInHours){
         System.out.println(name + "has accelerated with " + speed + " km/h for "
