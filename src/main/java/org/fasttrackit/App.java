56fc19828934ea12970c4507943f3293fdbeb3f1
mihai135 <mihai135@yahoo.com>
2019-01-17 19:38:47 +0200

    studied arrays

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -2,73 +2,70 @@ package org.fasttrackit;
 
 import org.fasttrackit.cheater.UFO;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
 
-        Engine engine = new Engine();
-        engine.manufacturer = "Renault";
-        engine.capacity = 1500;
+        Game game = new Game();
+        game.start();
 
-        Car car = new Car(engine);
-        car.setName("Dacia");
-        car.setColor("white");
-        car.doorCount = 4;
-        car.setMileage(7.5);
-        car.running = true;
-
-        System.out.println(car);
-
-        double distance = car.accelerate(100, 0.5);
-
-
-        car.engine.manufacturer = "BMW";
-
-        Car car2 = new Car(new Engine());
-
-
-//      Example of static variables unique for the whole application
-
-        Vehicle vehicle1 = new Vehicle();
-        vehicle1.vehicleCount = 1;
-
-        Vehicle vehicle2 = new Vehicle();
-        vehicle2.vehicleCount = 2;
-
-        Vehicle.vehicleCount = 3;
-
-        System.out.println("value from vehicle 1: " + vehicle1.vehicleCount);
-        System.out.println("value from vehicle 2: " + vehicle2.vehicleCount);
-        System.out.println("value from Vehicle class: " + Vehicle.vehicleCount);
-
-        new AutoVehicle();
-
-        new AutoVehicle(new Engine());
-
-        double acc = vehicle1.accelerate(100);
-        System.out.println(acc);
-
-        double acc2 = vehicle1.accelerate(100, 4);
-        System.out.println(acc2);
-
-        new UFO().accelerate(100,4);
-
-        //polymorphims (an object can take multiple forms)
-        Vehicle ufo = new UFO();
-        //the type of the variable does not determine object behaviour
-        ufo.accelerate(200, 2);
-
-        //the type of the variables determines what methods can be invoked
-        //we might need type casting (see below)
-        ((UFO) ufo).concealCheating();
-
-
-
+//        Engine engine = new Engine();
+//        engine.manufacturer = "Renault";
+//        engine.capacity = 1500;
+//
+//        Car car = new Car(engine);
+//        car.setName("Dacia");
+//        car.setColor("white");
+//        car.doorCount = 4;
+//        car.setMileage(7.5);
+//        car.running = true;
+//
+//        System.out.println(car);
+//
+//        double distance = car.accelerate(100, 0.5);
+//
+//
+//        car.engine.manufacturer = "BMW";
+//
+//        Car car2 = new Car(new Engine());
+//
+//
+////      Example of static variables unique for the whole application
+//
+//        Vehicle vehicle1 = new Vehicle();
+//        vehicle1.vehicleCount = 1;
+//
+//        Vehicle vehicle2 = new Vehicle();
+//        vehicle2.vehicleCount = 2;
+//
+//        Vehicle.vehicleCount = 3;
+//
+//        System.out.println("value from vehicle 1: " + vehicle1.vehicleCount);
+//        System.out.println("value from vehicle 2: " + vehicle2.vehicleCount);
+//        System.out.println("value from Vehicle class: " + Vehicle.vehicleCount);
+//
+//        new AutoVehicle();
+//
+//        new AutoVehicle(new Engine());
+//
+//        double acc = vehicle1.accelerate(100);
+//        System.out.println(acc);
+//
+//        double acc2 = vehicle1.accelerate(100, 4);
+//        System.out.println(acc2);
+//
+//        new UFO().accelerate(100,4);
+//
+//        //polymorphims (an object can take multiple forms)
+//        Vehicle ufo = new UFO();
+//        //the type of the variable does not determine object behaviour
+//        ufo.accelerate(200, 2);
+//
+//        //the type of the variables determines what methods can be invoked
+//        //we might need type casting (see below)
+//        ((UFO) ufo).concealCheating();
 
 
 
